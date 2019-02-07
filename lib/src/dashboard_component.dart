1085198e1255a00c83b6310f341388f48a92d9ee
Yintong <zhouyintong96@gmail.com>
2019-02-07 15:25:38 +0100

    add override and costructor

--- lib/src/dashboard_component.dart
+++ lib/src/dashboard_component.dart
@@ -1,4 +1,6 @@
 import 'package:angular/angular.dart';
+import 'hero.dart';
+import 'hero_service.dart';
 
 @Component(
   selector: 'my-dashboard',
@@ -6,6 +8,13 @@ import 'package:angular/angular.dart';
   templateUrl: 'dashboard_component.html',
    directives: [coreDirectives],
 )
-class DashboardComponent {
+class DashboardComponent implements OnInit {
+  List<Hero> heroes;
+  final HeroService _heroService;
+  DashboardComponent(this._heroService);
 
+  @override
+  void ngOnInit() async {
+    heroes = (await _heroService.getAll()).skip(1).take(4).toList();
+  }
 }
