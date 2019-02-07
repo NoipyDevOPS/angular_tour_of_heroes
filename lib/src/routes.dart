025a16638c7d611794e778ea79a7949c17d7d3fd
Yintong <zhouyintong96@gmail.com>
2019-02-07 15:43:41 +0100

    add hero template

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -2,22 +2,30 @@ import 'package:angular_router/angular_router.dart';
 import 'route_paths.dart';
 import 'hero_list_component.template.dart' as hero_list_template;
 import 'dashboard_component.template.dart' as dashboard_template;
+import 'hero_component.template.dart' as hero_template;
 
 export 'route_paths.dart';
 
 class Routes {
+  static final hero = RouteDefinition(
+    routePath: RoutePaths.hero,
+    component: hero_template.HeroCompoenentNgFactory,
+  );
+
   static final heroes = RouteDefinition(
-    routePath: RoutePath.heroes,
+    routePath: RoutePaths.heroes,
     component: hero_list_template.HeroListComponentNgFactory,
   );
 
   static final dashboard = RouteDefinition(
-    routePath: RoutePath.dashboard,
+    routePath: RoutePaths.dashboard,
     component: dashboard_template.DashboardComponentNgFactory,
   );
 
   static final all = <RouteDefinition>[
+    hero,
     heroes,
+    dashboard,
   // ···
   RouteDefinition.redirect(
     path: '',
