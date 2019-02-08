e742fd627d39285087e28791c220d0444c97a133
Yintong <zhouyintong96@gmail.com>
2019-02-08 09:52:11 +0100

    dashboard fix

--- lib/src/dashboard_component.dart
+++ lib/src/dashboard_component.dart
@@ -1,12 +1,13 @@
 import 'package:angular/angular.dart';
 import 'hero.dart';
 import 'hero_service.dart';
+import 'route_paths.dart';
 
 @Component(
   selector: 'my-dashboard',
   template: '<h3>Dashboard</h3>',
   templateUrl: 'dashboard_component.html',
-   directives: [coreDirectives],
+  directives: [coreDirectives],
 )
 class DashboardComponent implements OnInit {
   List<Hero> heroes;
@@ -17,4 +18,6 @@ class DashboardComponent implements OnInit {
   void ngOnInit() async {
     heroes = (await _heroService.getAll()).skip(1).take(4).toList();
   }
+
+  String heroUrl(int id) => RoutePaths.hero.toUrl(parameters: {idParam: '$id'});
 }
