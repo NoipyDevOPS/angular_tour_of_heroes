d079519de7450358ad9dced5934b09a7b93b199c
Yintong <zhouyintong96@gmail.com>
2019-02-08 11:23:15 +0100

    add new class

--- web/main.dart
+++ web/main.dart
@@ -1,12 +1,12 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:angular_tour_of_heroes/app_component.template.dart' as ng;
-import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
 import 'main.template.dart' as self;
 
 @GenerateInjector([
   routerProvidersHash, 
-  ClassProvider(BrowserClient),
+  ClassProvider(Client, useClass: InMemoryDataService),
 ])
 
 final InjectorFactory injector = self.injector$Injector;
