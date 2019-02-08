6e4ca2f23b34fe02963476b7872d533b69f54342
Yintong <zhouyintong96@gmail.com>
2019-02-08 10:58:56 +0100

    fix main

--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,13 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:angular_tour_of_heroes/app_component.template.dart' as ng;
+import 'package:http/browser_client.dart';
 import 'main.template.dart' as self;
 
-@GenerateInjector(
+@GenerateInjector([
   routerProvidersHash, 
-)
+  ClassProvider(BrowserClient),
+])
 
 final InjectorFactory injector = self.injector$Injector;
 
