3de1b7b4660c9e915a149dc7bbcf9f347ba41d9f
Yintong <zhouyintong96@gmail.com>
2019-02-19 16:50:40 +0100

    fix

--- lib/src/hero_list_component.dart
+++ lib/src/hero_list_component.dart
@@ -1,60 +1,49 @@
+import 'dart:async';
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+import 'route_paths.dart';
 import 'hero.dart';
 import 'hero_component.dart';
 import 'hero_service.dart';
-import 'package:angular_router/angular_router.dart';
+
 
 @Component(
   selector: 'my-heroes',
   templateUrl: 'hero_list_component.html',
   styleUrls: ['hero_list_component.css'],
-  template: '''
-  <div *ngIf="hero != null">
-    <h2>{{hero.name}}</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-  </div>''',
-  directives: [coreDirectives, HeroCompoenent],
-  providers: [ClassProvider(HeroService)],
+  directives: [coreDirectives, HeroComponent],
   pipes: [commonPipes],
 )
 
-class HeroListComponent implements OnInit{
-  final title = 'Tour of Heroes';
+class HeroListComponent implements OnInit {
   final HeroService _heroService;
   final Router _router;
-  //HeroService heroService = HeroService();
   List<Hero> heroes;
   Hero selected;
-
   HeroListComponent(this._heroService, this._router);
-
   Future<void> _getHeroes() async {
     heroes = await _heroService.getAll();
   }
 
-  void ngOnInit() => _getHeroes;
-
-  void onSelected(Hero hero) => selected = hero;
-  
-  String _heroUrl(int id) => RoutePaths.hero.toUrl(parameters: {idParam: '$id'});
-
-  Future<NavigationResult> gotoDetail() => _router.navigate(_heroUrl(selected.id));
-
   Future<void> add(String name) async {
-    name = name.trim();  
-    if(name.isEmpty) return null;
+    name = name.trim();
+    if (name.isEmpty) return null;
     heroes.add(await _heroService.create(name));
     selected = null;
   }
 
   Future<void> delete(Hero hero) async {
     await _heroService.delete(hero.id);
-    hero.remove(hero);
-    if(selected == hero) selected = null;
+    heroes.remove(hero);
+    if (selected == hero) selected = null;
   }
-}
 
+  void ngOnInit() => _getHeroes();
+  void onSelect(Hero hero) => selected = hero;
+
+  String _heroUrl(int id) =>
+      RoutePaths.hero.toUrl(parameters: {idParam: '$id'});
+
+  Future<NavigationResult> gotoDetail() =>
+      _router.navigate(_heroUrl(selected.id));
+}
