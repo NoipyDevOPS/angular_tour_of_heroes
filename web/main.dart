d09eb9278c42fd4e51a561cce52986c888d220d5
Yintong <zhouyintong96@gmail.com>
2019-02-19 16:53:05 +0100

    add package

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,7 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:angular_tour_of_heroes/app_component.template.dart' as ng;
+import 'package:angular_tour_of_heroes/in_memory_data_service.dart';
 import 'package:http/http.dart';
 import 'main.template.dart' as self;
 
