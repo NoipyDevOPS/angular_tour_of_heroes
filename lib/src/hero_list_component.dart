eb7e1036a39ff366c41f6f57071faea9cbe9d64e
Yintong <zhouyintong96@gmail.com>
2019-02-20 10:47:14 +0100

    fix

--- lib/src/hero_list_component.dart
+++ lib/src/hero_list_component.dart
@@ -14,13 +14,14 @@ import 'hero_service.dart';
   directives: [coreDirectives, HeroComponent],
   pipes: [commonPipes],
 )
-
 class HeroListComponent implements OnInit {
   final HeroService _heroService;
   final Router _router;
   List<Hero> heroes;
   Hero selected;
+
   HeroListComponent(this._heroService, this._router);
+
   Future<void> _getHeroes() async {
     heroes = await _heroService.getAll();
   }
@@ -39,6 +40,7 @@ class HeroListComponent implements OnInit {
   }
 
   void ngOnInit() => _getHeroes();
+
   void onSelect(Hero hero) => selected = hero;
 
   String _heroUrl(int id) =>
